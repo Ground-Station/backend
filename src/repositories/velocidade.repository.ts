8284a8bed603de48e919c335e8e61ea3cc9e033a
Gabriela <gabrielaalvesdagama@gmail.com>
2020-11-20 17:24:17 -0300

    Adding valocity calculation
    Signed-off-by: Gabriela <gabrielaalvesdagama@gmail.com>
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>

--- src/repositories/velocidade.repository.ts
+++ src/repositories/velocidade.repository.ts
@@ -2,15 +2,30 @@ import {DefaultCrudRepository} from '@loopback/repository';
 import {Velocidade, VelocidadeRelations} from '../models';
 import {MongoDsDataSource} from '../datasources';
 import {inject} from '@loopback/core';
+import * as moment from 'moment'
+
+
+
 
 export class VelocidadeRepository extends DefaultCrudRepository<
   Velocidade,
   typeof Velocidade.prototype.id,
   VelocidadeRelations
 > {
+
   constructor(
     @inject('datasources.mongoDS') dataSource: MongoDsDataSource,
   ) {
     super(Velocidade, dataSource);
   }
+
+  public calculaVelocidade(altitude_inical: number, altitude_final: number, tempo_inicial: Date, tempo_final: Date) {
+    
+    let inicial = tempo_inicial.fn.toString()
+    let time_variation =  moment.duration(tempo_final.diff())
+      return (altitude_final - altitude_inical) / 
+  }
+
+
+
 }
